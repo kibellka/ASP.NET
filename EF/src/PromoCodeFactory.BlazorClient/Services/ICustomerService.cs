93015b4b37fa0b31a4c59cc29bdecbb30e38f128
Yuliya Kuzmenko <YuliyaKuzmenko@example.com>
2025-02-20 22:16:43 +0300

    Пишем свой API: gRPC

---
+++ EF/src/PromoCodeFactory.BlazorClient/Services/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using PromoCodeFactory.BlazorClient.Models;
+
+namespace PromoCodeFactory.BlazorClient.Services;
+
+public interface ICustomerService
+{
+    Task<ICollection<Customer>> GetAllCustomersAsync();
+
+    Task<string> CreateCustomerAsync(Customer customer);
+
+    Task<string> UpdateCustomerAsync(Customer customer);
+
+    Task<string> DeleteCustomerAsync(string id);
+}
